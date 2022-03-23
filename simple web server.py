962851739db3dd8023b64ab89d4910aa03617346
KalashnikovEgor <39344245+KalashnikovEgor@users.noreply.github.com>
2022-03-24 01:34:41 +0300

    Update simple web server.py

--- simple web server.py
+++ simple web server.py
@@ -1,17 +1,49 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import time
 
-from flask import Flask, request
+hostName = "localhost"
+serverPort = 8080
 
-app = Flask(__name__)
+class MyServer(BaseHTTPRequestHandler):
 
-@app.route('/')
-def func():
-    name = request.args.get('name')
-    message = request.args.get('message')
-    if name == None or message == None:
-        return 'Наберите get запрос (/?name=Rekruto&message=Давай дружить!) и всё заработает <br> Надеюсь я правильно понял задание :)'
-    return f"Hello {name}! <br> {message}"
-
+    # def parse_url(self):
+    #     request = self.path
+    #     print(request)
+    #     parsed_url = urlparse(request)
+    #     dict = parse_qs(parsed_url.query)
+    #     return dict
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        # self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        request = self.path
+        parsed_url = urlparse(request)
+        dict = parse_qs(parsed_url.query)
+        if 'name' in dict and 'message' in dict:
+            name = dict['name']
+            message = dict['message']
+            self.wfile.write(bytes("<p>Hello  %s</p>" % name[0], "utf-16"))
+            self.wfile.write(bytes(message[0], "utf-16"))
+        else:
+            self.wfile.write(bytes("Наберите get запрос (/?name=Rekruto&message=Давай дружить!) и всё заработает <br> Надеюсь я правильно понял задание :)", "utf-16"))
+
+
+
+
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
